4635427d6e4b834f8a227d1a3696956618e3cbc1
Karthick47v2 <Karthicksharma11@gmail.com>
2022-09-29 18:58:52 +0530

    refactor & add docstring

--- src/lang.py
+++ src/lang.py
@@ -1,43 +1,66 @@
+"""spelling and grammer checker"""
 import language_tool_python
 
 
 class LangChecker:
+    # pylint: disable=too-few-public-methods
+    """Class for spelling and grammer checking"""
+
     def __init__(self, corpus):
+        """Initialize checker.
+
+        Args:
+            corpus (listlist((str))): input text.
+        """
         self.__lang_tool = language_tool_python.LanguageTool('en-US')
         self.__disabled_rules = ['ARROWS', 'WHITESPACE_RULE', 'EN_COMPOUNDS']
-        self.__mistake = []
-        self.__mistake_exp = []
-        self.__top3_suggestions = []
+
+        self.__suggestions = []
 
         self.__analyze_txt(corpus)
 
         self.__lang_tool.close()
 
-    def __append_error(self, rule, mistake, mistake_exp, suggestions):
+    def __append_error(self, rule, suggestions, idx):
+        """Add mistake and it info to list.
+
+        Args:
+            rule (dict[str,str]): dictionary of mistake's details.
+            suggestions (list(dict[str, str])): list of rules.
+            idx (int): slide no (starting from 0).
+        """
         if len(rule.replacements) > 0 and rule.ruleId not in self.__disabled_rules:
             if rule.context[rule.offset: rule.offset + 1].isalnum():
-                mistake.append(
-                    rule.context[rule.offset: rule.errorLength + rule.offset])
-                mistake_exp.append(rule.message)
-                suggestions.append(rule.replacements[:3])
+                suggestions.append({
+                    "mistake":
+                    rule.context[rule.offset: rule.errorLength + rule.offset],
+                    "exp": rule.message,
+                    "top3": rule.replacements[:3],
+                    "id": idx + 1})
 
     def __analyze_txt(self, corpus):
-        for slide_txt in corpus:
-            slide_mistake = []
-            slide_mistake_exp = []
-            slide_top3_suggestions = []
+        """Iterate through text and find errors.
+
+        Args:
+            corpus (listlist((str))): input text.
+        """
+        for idx, slide_txt in enumerate(corpus):
+            slide_suggestions = []
 
             for txt in slide_txt:
                 if len(txt) > 2:
                     predict = self.__lang_tool.check(txt)
                     for rule in predict:
                         self.__append_error(
-                            rule, slide_mistake, slide_mistake_exp, slide_top3_suggestions)
+                            rule, slide_suggestions, idx)
 
-            if len(slide_mistake) > 0:
-                self.__mistake.append(slide_mistake)
-                self.__mistake_exp.append(slide_mistake_exp)
-                self.__top3_suggestions.append(slide_top3_suggestions)
+            if len(slide_suggestions) > 0:
+                self.__suggestions.append(slide_suggestions)
 
     def get_results(self):
-        return self.__mistake, self.__mistake_exp, self.__top3_suggestions
+        """Return collected errors and its suggestions.
+
+        Returns:
+            list(list(dict[str, str])): list of rules for each slide.
+        """
+        return self.__suggestions
--- src/slide.py
+++ src/slide.py
@@ -1,16 +1,26 @@
+"""Extract details from slides"""
+
 from urllib.error import HTTPError
 from urllib import request
-from pptx import Presentation
 from collections import Counter
+
+from pptx import Presentation
 
 
 class Slide:
-    __PPTX_NAME = 'test.pptx'
+    """Class for analyzing presentation slide"""
 
-    def __init__(self, url):
+    def __init__(self, url, name):
+        """Initialize slide details
+
+        Args:
+            url (str): shared url
+            name (str): name for slide
+        """
         self.__url = url.split('/')
         self.__url[-1] = 'export/pptx'
         self.__url = '/'.join(self.__url)
+        self.filename = name
 
         self.__txt_lst = []
         self.__word_count = []
@@ -21,31 +31,54 @@ class Slide:
         self.__load_pptx()
 
     def __load_pptx(self):
+        """Download slide from source
+
+        Returns:
+            dict[str, str]: error status if anything happens
+        """
         try:
-            request.urlretrieve(self.__url, Slide.__PPTX_NAME)
+            request.urlretrieve(self.__url, self.filename)
         except HTTPError:
-            return{
-                'stauts': 400
+            return {
+                'status': 400
             }
 
     def __set_slide_count(self, slides):
+        """Update slide count
+
+        Args:
+            slides (obj): pptx slides object
+        """
         self.__slide_count = len(slides)
 
     def __extract_txt(self, runs, word_count, txt):
+        """Extract text from each slide
+
+        Args:
+            runs (obj): paragraph object
+            word_count (list[int]): list of word count per para
+            txt (list[str]): list of text per para
+        """
         for run in runs:
-            if (run.font.name is not None):
+            if run.font.name is not None:
                 self.__font_lst.append(run.font.name)
 
             word_count.append(
                 len(run.text.split(' ')))
             txt.append(run.text)
 
-    def __increment_shape_count(self, type):
-        if type == 13 or type == 6:
+    def __increment_shape_count(self, shape_type):
+        """Update shape count
+
+        Args:
+            shape_type (int): type of shape (picture = 13, auto-shape = 6, etc.)
+        """
+        if shape_type == 13 or shape_type == 6:
             self.__shape_count += 1
 
     def extract_pptx(self):
-        with open(Slide.__PPTX_NAME, 'rb') as pptx:
+        """Extract required details from slide (text, shape count, word count, etc.)"""
+        with open(self.filename, 'rb') as pptx:
             prs = Presentation(pptx)
             self.__set_slide_count(prs.slides)
 
@@ -64,7 +97,21 @@ class Slide:
                 self.__word_count.append(slide_word_count)
 
     def get_txt(self):
+        """Return all text from pptx
+
+        Returns:
+            list(list(str)): list of text for each slide
+        """
         return self.__txt_lst
 
     def get_data(self):
-        return self.__slide_count, self.__word_count, self.__shape_count, dict(Counter(self.__font_lst))
+        """Return infomation about slide other than text.
+
+        Returns:
+            Tuple(int, list(list(int)), int, dict(str: int)): tuple of number of slides,
+            word count per slide, shape count, unique value count of fonts
+        """
+        return (self.__slide_count,
+                self.__word_count,
+                self.__shape_count,
+                dict(Counter(self.__font_lst)))
